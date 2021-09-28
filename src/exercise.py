97f92901f81191bf879ee5031c0f7d363eb140c4
Jake Lee <jake.lee@fdmgroup.com>
2021-09-28 17:04:08 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    nums = []
+    while True:
+        num = int(input())
+        if (num == -1):
+            break
+        else:
+            nums.append(num)
+    search = int(input("Search for?"))
+    for i in range(len(nums)):
+        if (nums[i] == search):
+            print(str(search) + " is at index " + str(i))
 
 if __name__ == '__main__':
     main()
